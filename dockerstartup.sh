5ff4290b188e433315eea1e8fa0cde9f246de85b
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2021-06-15 16:51:32 -0400

    Create dockerstartup.sh

---
+++ dockerstartup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#CONTAINER=$(docker container list -a | awk '{print $14}')
+
+#Start the download of the docker container
+#sudo docker run -ti cyberxsecurity/ansible bash
+
+#display known containers
+#docker container list -a | awk '{print $14}'
+
+#Start the container and attach the container
+#sudo docker start ${CONTAINER} && sudo docker attach ${CONTAINER}
