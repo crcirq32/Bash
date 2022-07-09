77327f61f1e793a4cd4859d4918bb38cc3787389
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2022-07-08 18:23:28 -0700

    Update jupyternotebook.sh

--- jupyternotebook.sh
+++ jupyternotebook.sh
@@ -10,9 +10,7 @@ echo $?
 sleep 10
 
 #install repo's
-sudo apt-get install python3-pip python3-dev -y
-&& pip3 -H install --upgrade pip
-&& pip3 -H install virtualenv 
+sudo apt-get install python3-pip python3-dev -y &&  pip3 -H install --upgrade pip && pip3 -H install virtualenv 
 
 echo $?
 sleep 10
