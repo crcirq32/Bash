61a43199a06a1fb0f453e6c0e19b23414aae9cf2
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2022-07-09 09:55:44 -0700

    Update jupyternotebook.sh

--- jupyternotebook.sh
+++ jupyternotebook.sh
@@ -6,22 +6,15 @@ sudo mkdir -p ~/Desktop/notebook && sudo chmod 777 ~/Desktop/notebook && cd ~/De
 #update
 sudo apt-get update && sudo apt-get -y upgrade &
 
+sleep 10
+
+sudo apt-get install python3-pip python3-dev && sudo -H pip3 install --upgrade pip \
+&& sudo -H pip3 install virtualenv \
+&& virtualenv jupyterenv && source jupyterenv/bin/activate && \
+pip install numpy matplotlib jupyter scipy requests graphviz
+
 echo $?
-sleep 10
-
-#install repo's
-sudo apt-get install python3-pip python3-dev -y &&  pip3 -H install --upgrade pip && pip3 -H install virtualenv 
-
-echo $?
-sleep 10
-
-#Start env && activate env
-virtualenv jupyterenv && source jupyterenv/bin/activate 
-
-#install library's into env
-pip3 install numpy matplotlib jupyter scipy graphviz sklearn pandas
-
-sleep 10
+sleep 15
 
 #start notebook
 jupyter notebook --ip="127.0.0.1" --port=8888 --allow-root
