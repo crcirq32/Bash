4344cde2cd772704eb146f5dfb3327ced40806ac
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2022-11-11 18:46:02 -0800

    Create remnuxcli.sh

---
+++ remnuxcli.sh
@@ -0,0 +1,15 @@
+#https://docs.remnux.org/install-distro/add-to-existing-system
+
+#wget https://REMnux.org/remnux-cli
+
+#sha256sum remnux-cli
+
+#mv remnux-cli remnux
+#chmod +x remnux
+#sudo mv remnux /usr/local/bin
+
+#sudo remnux install --mode=addon
+#sudo reboot
+
+#Docker
+#https://docs.remnux.org/install-distro/remnux-as-a-container
