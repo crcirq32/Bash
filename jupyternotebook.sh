6a19c3b780ecbac7e69f21e2f46a561c2a9918fd
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2022-12-29 16:43:58 -0500

    Update jupyternotebook.sh

--- jupyternotebook.sh
+++ jupyternotebook.sh
@@ -8,9 +8,14 @@ sudo apt-get update -y && sudo apt-get -y upgrade &
 
 sleep 10
 
+#install deps
 sudo apt-get install python3-pip python3-dev -y && -H pip3 install --upgrade pip -y \
 && -H pip3 install virtualenv \
+
+#start py env
 && virtualenv jupyterenv && source jupyterenv/bin/activate && \
+
+#add pip libs to py env
 pip install numpy matplotlib jupyter scipy requests graphviz
 
 echo $?
